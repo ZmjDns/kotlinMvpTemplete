bb3ae0b9fcdb17bb637a886aea44f602c2ff3e8e
ZmjDns <292570927@qq.com>
2019-09-13 21:13:05 +0800

    状态栏

---
+++ BaseLibrary/src/main/java/com/zmj/baselibrary/view/StatusBarView.kt
@@ -0,0 +1,19 @@
+package com.zmj.baselibrary.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/9/12
+ * Description :
+ */
+class StatusBarView : View {
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context): super(context){}
+}
